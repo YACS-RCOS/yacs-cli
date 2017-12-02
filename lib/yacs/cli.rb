f05575f676337b64f1c3662b01c6f2cf8722bee2
Richie <richiejoeyoung@gmail.com>
2017-12-02 15:09:46 -0500

    add multiple field search

--- lib/yacs/cli.rb
+++ lib/yacs/cli.rb
@@ -1,13 +1,28 @@
 require "thor"
 require "yacs/cli/version"
 require "yacs/cli/course"
+require "yacs/client"
+require "yacs/cli/printer"
 
 module Yacs
   module Cli
     class Command < Thor
+      COURSE_API_OPTIONS = %w(id name number department_code department_id search)
+      SYSTEM_OPTIONS = %w(v q)
     
-      desc "course COMMANDS", "find and browse courses"
-      subcommand "course", Yacs::Cli::Course
+      desc "course", "find and browse courses"
+      COURSE_API_OPTIONS.each { |o| option o }
+      SYSTEM_OPTIONS.each { |o| option o}
+
+      def course
+        courses = Yacs::Client.new.courses(options)
+        fields = %w(id)
+        unless options['q']
+          fields << 'name'
+          fields << 'description' if options['v']
+        end
+        Printer.print courses, fields
+      end
     end
   end
 end
